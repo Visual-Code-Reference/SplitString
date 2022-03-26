6d7c6bb2bc6158856bc22e506125f4927270b2f2
John Sartain <jhnsartain3@gmail.com>
2022-03-25 21:24:59 -0500

    Add project files.

---
+++ SplitString/Program.cs
@@ -0,0 +1,18 @@
+﻿string sentence = "The quick brown fox jumps over the lazy dog.";
+string names = "John,Alex,Sam,Brad,Tom";
+string emails = "John@Mail.com;Alex@Mail.com;Sam@Mail.com;Brad@Mail.com;Tom@Mail.com";
+
+string[] words = sentence.Split(' ');
+
+foreach (var item in words)
+    Console.WriteLine(item);
+
+string[] nameList = names.Split(',');
+
+foreach (var item in nameList)
+    Console.WriteLine(item);
+
+string[] emailList = emails.Split(';');
+
+foreach (var item in emailList)
+    Console.WriteLine(item);
